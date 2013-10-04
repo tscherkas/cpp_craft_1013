1f28cfcdf237dc03a94d4bc9b5c130a1fceab89c
ihar.marozau <ihar.marozau@disfa.com>
2013-10-04 09:47:30 +0300

    reviewed presentations, added examples and course programme

--- lections/code_examples/001_cmake_example/sources/write_file/main.cpp
+++ lections/code_examples/001_cmake_example/sources/write_file/main.cpp
@@ -6,4 +6,9 @@ int main( int argc, char* argv[] )
 {
 	std::ofstream output_file( BINARY_DIR "/output_example.txt" );
 	output_file << "out put file" << std::endl;
+	output_file.close();
+	
+	std::fstream another_output_file( BINARY_DIR "/output_example.txt", std::fstream::out | std::fstream::app );
+	another_output_file << "appended line" << std::endl;
+	another_output_file.close();
 }
