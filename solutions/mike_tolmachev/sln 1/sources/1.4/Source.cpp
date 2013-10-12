4f32d6986b07aa91d432f2836b2f5da6790fdbf3
fobostm <fobostm@gmail.com>
2013-10-13 02:20:08 +0300

    added cmake.

---
+++ solutions/mike_tolmachev/sln 1/sources/1.4/Source.cpp
@@ -0,0 +1,38 @@
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <cmath>
+
+
+int main()
+{
+	std::ifstream in(SOURCE_DIR "/input.txt");
+	std::ofstream out(BINARY_DIR "/output.txt");
+
+	if (in.is_open() && out.is_open())
+	{
+		int N;
+		in >> N;
+
+		double code;
+		std::set<unsigned int> codes;
+		for (int i = 0; i < N; ++i)
+		{
+			in >> code;
+			unsigned int tmp = code*10000.0;
+			codes.insert(tmp);
+		}
+
+		
+		while(in >> code)
+		{
+			unsigned int tmp = code*10000.0;
+
+			out << ((codes.find(tmp) != codes.end()) ?  "YES" : "NO") << std::endl;
+		}
+	}
+
+    in.close();
+    out.close();
+}
