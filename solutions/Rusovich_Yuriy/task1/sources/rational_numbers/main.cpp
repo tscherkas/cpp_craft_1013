a2b05a9c50f4bb27adb16b2b1892ec90373938dd
Yura <y.rusovich@gmail.com>
2013-10-13 22:26:58 +0300

    added second exercise from first task from Rusovich Yuriy

---
+++ solutions/Rusovich_Yuriy/task1/sources/rational_numbers/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+#define accuracy 10000
+
+int main()
+{
+	std::ifstream inputFile(SOURCE_DIR "/input_rational_numbers.txt");
+	std::ofstream outputFile(SOURCE_DIR "/output_rational_numbers.txt" );
+	
+	int countKey = 0;
+	std::string countKeyString;
+	std::vector < int > keys; 
+
+	std::getline(inputFile, countKeyString);
+	countKey = atoi(countKeyString.c_str());
+	
+	for (int i = 0; i < countKey; ++i)
+	{
+		std::string key;
+		if (!inputFile.eof())
+		{
+			std::getline(inputFile, key);
+			keys.push_back(atof(key.c_str()) * accuracy);
+		} else 
+		{
+			break;
+		}
+	}
+	
+	while (!inputFile.eof()) 
+    {
+		std::string passwordString;
+		std::getline(inputFile, passwordString);
+		
+		if (passwordString.empty())
+			continue;
+
+		int password = atof(passwordString.c_str()) * accuracy;
+		
+		for (int i = 0; i < countKey; ++i)
+		{
+			if (password == keys[i])
+			{
+				outputFile << std::string("YES") << std::endl;
+				break;
+			} else
+			{
+				if (i + 1 == countKey)
+					outputFile << std::string("NO") << std::endl;
+				continue;
+			}
+		}
+	}
+
+	inputFile.close();
+	outputFile.close();
+    
+	system("pause");
+	return 0;
+}
