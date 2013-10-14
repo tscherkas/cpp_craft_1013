570d5854e579da333133e7d5c1b6cd3fb8c2489a
sorca <sorca.by@gmail.com>
2013-10-14 18:26:06 +0300

    solved task 1 and 2.

---
+++ solutions/sorca.by/HomeWork/1_1-build/CMakeFiles/1_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/1_1.dir/main.cpp.o"
+  "1_1.pdb"
+  "1_1"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/1_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ solutions/sorca.by/HomeWork/1_2-build/CMakeFiles/1_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/1_2.dir/main.cpp.o"
+  "1_2.pdb"
+  "1_2"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/1_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
