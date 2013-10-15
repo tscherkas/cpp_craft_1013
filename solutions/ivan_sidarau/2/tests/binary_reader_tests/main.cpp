d7c42679d9ecb3fb1b86151f0dd7e973bb2583f5
Ivan Sidarau <ivan.sidarau@disfa.com>
2013-10-15 13:18:05 +0300

    adding example for second task solution

---
+++ solutions/ivan_sidarau/2/tests/binary_reader_tests/main.cpp
@@ -0,0 +1,20 @@
+#include <reader.h>
+
+#include <iostream>
+
+#define test_no_throw( action ) \
+{ \
+	try \
+	{ \
+		action; \
+	} \
+	catch( ... ) \
+	{ \
+		std::cout << "test_no_throw failed: " <<  __FILE__ << ":" << __LINE__ << std::endl; \
+	} \
+} \
+
+int main()
+{
+	test_no_throw( binary_reader::proxy_function_that_was_created_only_for_compiling_goals() );
+}
