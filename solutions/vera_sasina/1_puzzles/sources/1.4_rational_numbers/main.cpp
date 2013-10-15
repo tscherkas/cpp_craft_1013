d9729deb4d1513c4df1134ad7bf4423b15ad31d3
Vera Sasina <vera.sasina@gmail.com>
2013-10-15 10:40:50 +0300

    fix 1.4 task

--- solutions/vera_sasina/1_puzzles/sources/1.4_rational_numbers/main.cpp
+++ solutions/vera_sasina/1_puzzles/sources/1.4_rational_numbers/main.cpp
@@ -4,64 +4,46 @@
 #include <string>
 
 using namespace std;
+typedef unsigned long long ull;
 
 int main( int argc, char* argv[] )
 {
-	//open files for read and write
 	ifstream input_file( "input.txt" );
 	ofstream output_file( "output.txt" );
-	int count_array, pos;
-	input_file >> count_array;	// read size of array
-	if(count_array < 1)
+	int size_array, pos;
+	input_file >> size_array;	// read size of array
+	if(size_array < 1)
 		return 0;
-	vector<float>array_numb;	// vector for rational numbers
-	float num;					// temp variables
+	vector<ull>array_numb;	// vector for rational numbers
+	double num;					// temp variables
+	ull temp;
 	bool check;
-	string line;
 
 	//read values of array
-	for(; array_numb.size() < count_array; )
-	{
-		getline( input_file, line );		//read line from file
-		if(line.empty())					//skipp emty line
-			continue;
-		pos = line.find('.');				//set the precision
-		if(line.size() > pos+5)
-			line.erase(pos+5);	
-		num = stod(line, string::size_type());
-		array_numb.push_back(num);
+	for(int i = 0; i < size_array; i++)
+	{	
+		input_file >> num;
+		temp = num*10000;
+		array_numb.push_back(temp);
 	}
 	//read passwords for array from file
-	for( ; !input_file.eof(); )
+	while(!input_file.eof())
 	{
-		getline( input_file, line );		//read line from file
-		if(line.empty())					//skipp emty line
-			continue;
-		pos = line.find('.');				//set the precision
-		if(line.size() > pos+5)
-			line.erase(pos+5);	
-		num = stod(line, string::size_type());
-
-		array_numb.begin();					//go to begin of array
-		//comparison of numbers and passwords
-		for(int i = 0; i < count_array; i++)
+		input_file >> num;
+		for(vector<ull>::iterator it = array_numb.begin(); it != array_numb.end(); it++)
 		{
-			if(array_numb.at(i) == num)
-			{
-				check = true;
+			temp = num*10000;
+			check = *it == temp;
+			if(check)
 				break;
-			}
-			else
-				check = false;
 		}
-		//output to file
+
 		if(check)
 			output_file << "YES" << endl;
 		else
 			output_file << "NO" << endl;
 		
 	}
-	//close of files
 	input_file.close();
 	output_file.close();
 	return 0;
