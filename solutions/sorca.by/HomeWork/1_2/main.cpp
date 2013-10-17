7ad14ea4dc5ed1b8fed6fa741e08892a52daf68f
sorca <sorca.by@gmail.com>
2013-10-17 15:27:53 +0300

    Corrected mistakes
    Corrected "isEquals" function in 1_2

--- solutions/sorca.by/HomeWork/1_2/main.cpp
+++ solutions/sorca.by/HomeWork/1_2/main.cpp
@@ -5,57 +5,60 @@
 #include <vector>
 #include <algorithm>
 
-bool isEquals (double a, double b, double eps);
+bool isEquals (double a, double b, double degree);
+long long int myRound (double number, double degree);
 
 int main()
 {
-	static const double eps = 0.00001;
-	static const std::string yes = "YES";
-	static const std::string no = "NO";
-	double pass;
+    static const double degree = 10000;
+    static const std::string yes = "YES";
+    static const std::string no = "NO";
+    long n = 1;
+    double pass;
 
-	//open
-	std::ifstream inFile	(BINARY_DIR "input.txt");
-	std::ofstream outFile	(BINARY_DIR "output.txt");
+    //open
+    std::ifstream inFile	(BINARY_DIR "input.txt");
+    std::ofstream outFile	(BINARY_DIR "output.txt");
 
-	long N = 1;
-	inFile >> N;
-	if (N <= 0) {
-		return 1;
-	}
+    inFile >> n;
+    if (n <= 0 || !inFile.is_open()) {
+        return 1;
+    }
 
-	double  *codes = new double [N];
-	for (int i = 0; i<N; ++i){
-		inFile >> codes[i];
-	}
+    double  *codes = new double [n];
+    for (int i = 0; i<n; ++i){
+        inFile >> codes[i];
+    }
 
-	//should be optimized for "std::binary_search"
-	bool eqNum = false;
-	while(true) {
-		inFile >> pass;
-		if(inFile.eof())
-			break;
-		eqNum = false;
-		for(int i = 0; i < N; ++i){
-			if(isEquals(pass, codes[i], eps)) {
-				eqNum = true;
-				break;
-			}
-		}
-		outFile << (eqNum ? yes : no) << std::endl;
-		if(inFile.eof()) {
-			break;
-		}
-	}
-	//close
-	delete[] codes;
-	outFile.close();
-	inFile.close();
-	return 0;
+    //should be optimized for "std::binary_search"
+    bool eqNum = false;
+    while(true) {
+        inFile >> pass;
+        if(inFile.eof())
+            break;
+        eqNum = false;
+        for(int i = 0; i < n; ++i){
+            if(isEquals(pass, codes[i], degree)) {
+                eqNum = true;
+                break;
+            }
+        }
+        outFile << (eqNum ? yes : no) << std::endl;
+    }
+    //close
+    delete[] codes;
+    outFile.close();
+    inFile.close();
+    return 0;
 }
 
 
-bool isEquals(double a, double b, double eps) {
-	return fabs(a-b) < eps;
+bool isEquals(double a, double b, double degree) {
+    return myRound(a, degree) == myRound(b, degree);
 }
 
+long long int myRound (double number, double degree) {
+    number *= degree;
+    number = static_cast<long long int>(number);
+    return number;
+}
