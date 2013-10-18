a7e8b7bd97d8340761c2de224196160b159b09e9
TereschenkovaEugeniya <zhenya.tereschenkova@gmail.com>
2013-10-18 12:12:13 +0300

    Create 1_3.cpp
    solution 1_3

---
+++ solutions/Eugeniya_Tereschenkova/1/1_3/1_3.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <locale>
+#include <algorithm>
+using namespace std;
+
+bool isSpaceSymb(const char& symb)
+{
+	switch(symb)
+	{
+	case ' ':
+	case '-':
+	case '\\':
+		return 1;
+	default:
+		return 0;
+	}
+}
+
+bool formatting_line(ifstream& stream, string& line)
+{
+	bool bReturn = true;
+	const size_t maxInputLen = 100000;
+	getline(stream, line);
+	if(line.empty())
+		return bReturn;
+	if(line.size() > maxInputLen)
+		cout << "Could not open input file" << endl;
+	line.erase(remove_if(line.begin(), line.end(), isSpaceSymb), line.end());
+	transform(line.begin(), line.end(), line.begin(), tolower);
+	return bReturn;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	ifstream input_file(SOURCE_DIR "/Input.txt" );
+	ofstream output_file(SOURCE_DIR "/Output.txt" );
+	string line, key;
+	if(!input_file.is_open())
+	{
+		cout << "Could not open input file" << endl;
+		return 1;
+	}
+	if(!output_file.is_open())
+	{
+		cout << "Could not open output file" << endl;
+		input_file.close();
+		return 1;
+	}
+	if(!input_file.eof())
+		if(!formatting_line(input_file, line))
+		{
+			input_file.close();
+			output_file.close();
+			return 1;
+		}
+
+	while(!input_file.eof())
+	{
+		formatting_line(input_file, key);
+		if(key.empty())
+			continue;
+		reverse(key.begin(), key.end());
+		if (line.find(key) != string::npos)
+			output_file << "YES" << endl;
+		else
+			output_file << "NO" << endl;
+	}
+
+	input_file.close();
+	output_file.close();
+	
+	return 0;
+}
