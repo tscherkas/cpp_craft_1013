591971adde55664f9c0f72c8d71a3d89fc6a1250
tscherkas <itscherkas@gmail.com>
2013-10-20 21:29:19 +0300

    Added task2.4 v.2

---
+++ solutions/tscherkas/2/sources/2_4/marketevent.h
@@ -0,0 +1,32 @@
+#ifndef MARKETEVENT_H
+#define MARKETEVENT_H
+
+#include "inttypes.h"
+#include <iostream>
+#include <list>
+
+
+class MarketEvent
+{
+private:
+	uint32_t TYPE;
+	uint32_t TIME;
+	uint32_t LEN;
+	char* MSG;
+	MarketEvent();
+	MarketEvent& operator=(const MarketEvent&);
+public:
+	MarketEvent(uint32_t tp, uint32_t tm,uint32_t len,char* msg);	
+	MarketEvent(const MarketEvent& ev);	
+	~MarketEvent();
+	uint32_t getTime() const;
+	uint32_t getType() const;
+	uint32_t getLen() const;
+	std::string toString() const;
+	bool operator< (const MarketEvent& ev) const;
+	friend std::ostream& operator<< ( std::ostream& out, const MarketEvent& ev );
+	friend std::istream& operator>> ( std::istream& in,  MarketEvent& ev );
+
+};
+
+#endif //MARKETEVENT_H
