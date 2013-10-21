6c22f8c99c599e8dd7cda23b7969bb5007e1a6bb
Serg Brych <sbrych@gmail.com>
2013-10-21 10:34:39 +0300

    Put into file double result. Fixed open input and output files from BINARY_DIR.

--- solutions/serg_brych/2/tasks/sources/small_buffer/small_buffer.cpp
+++ solutions/serg_brych/2/tasks/sources/small_buffer/small_buffer.cpp
@@ -22,21 +22,22 @@ struct Buffer
 };
 
 
-// type std::pair<count_of_seconds, saved_messages>
-typedef std::map<uint32_t, std::pair<uint32_t, uint32_t> >  CountOfSavedMessage;
-// type std::pair<free_memory, saved_messages>
+// type std::map<type, count_of_saved_messages>
+typedef std::map<uint32_t, uint32_t >  CountOfSavedMessage;
+
 typedef std::map<uint32_t, Buffer >  CountOfSavedMessageTimed;
 
 int main() 
 { 
 	Package current_package ={0};
 	uint32_t current_time = 0;
+	uint32_t all_time = 0;
 	
 	CountOfSavedMessage all_saved_message;
 	CountOfSavedMessageTimed msg_of_time;
 
-	std::ifstream input_file(BINARY_DIR"input.txt", std::ifstream::binary);
-	std::ofstream output_file(BINARY_DIR"output.txt", std::ifstream::binary);
+	std::ifstream input_file(BINARY_DIR"\\input.txt", std::ifstream::binary);
+	std::ofstream output_file(BINARY_DIR"\\output.txt", std::ifstream::binary);
 	
 	if(input_file.is_open() && output_file.is_open())
 	{
@@ -50,8 +51,7 @@ int main()
 
 					std::for_each(msg_of_time.begin(), msg_of_time.end(), [&all_saved_message](CountOfSavedMessageTimed::value_type & i)
 					{
-						all_saved_message[i.first].first++;
-						all_saved_message[i.first].second += (i.second.count_of_stored_msg);
+						all_saved_message[i.first] += (i.second.count_of_stored_msg);
 					});
 
 					msg_of_time.clear();
@@ -68,18 +68,18 @@ int main()
 				std::streampos pos = input_file.tellg();
 				input_file.seekg(pos + static_cast<std::streamoff>(current_package.len));	
 			}
+			all_time++;
 		}
 		
 		std::for_each(msg_of_time.begin(), msg_of_time.end(), [&all_saved_message](CountOfSavedMessageTimed::value_type & i)
 		{
-			all_saved_message[i.first].first++;
-			all_saved_message[i.first].second += (i.second.count_of_stored_msg);
+			all_saved_message[i.first] += (i.second.count_of_stored_msg);
 		});
 		
-		std::for_each(all_saved_message.begin(), all_saved_message.end(), [&output_file] (CountOfSavedMessage::value_type & i)
+		std::for_each(all_saved_message.begin(), all_saved_message.end(), [&output_file, &all_time] (CountOfSavedMessage::value_type & i)
 		{
 			output_file.write(reinterpret_cast<const char*>(&(i.first)), sizeof(i.first));
-			uint32_t result = i.second.second / i.second.first;
+			double result = (static_cast<double>(i.second) / static_cast<double>(all_time));
 			output_file.write(reinterpret_cast<const char*>(&result), sizeof(result));
 		});
 
