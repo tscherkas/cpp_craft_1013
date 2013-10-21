8b5c22af606b58273671b6df0e1da297f6c4a07d
alstrokov <strokov.aleksandr@gmail.com>
2013-10-21 12:54:43 +0300

    task26 revision 0

--- solutions/al_strokov/homeworks/sources/26/main.cpp
+++ solutions/al_strokov/homeworks/sources/26/main.cpp
@@ -1,113 +1,106 @@
 #include <iostream>
 #include <fstream>
 #include <stdint.h>
-#include <set>
-#include <map>
 #include <string>
-#include <vector>
+#include <stdlib.h>
 
-struct dataStruct_t {
-	uint32_t type;
-	uint32_t time;
-	std::string msg;
-};
+uint32_t ymd2days(const std::string& ymd){
+	static const unsigned int daysInYear = 372;
+	static const unsigned int daysInMonth = 31;
+
+	unsigned int year;
+	unsigned int month;
+	unsigned int day;
+
+	year = atoi(ymd.substr(0, 4).c_str());
+	month = atoi(ymd.substr(4, 2).c_str());
+	day = atoi(ymd.substr(6, 2).c_str());
+
+	return ( (year - 1) * daysInYear + (month - 1) * daysInMonth + day );
+}
 
 int main(int argc, char** argv) {
-	std::cout << "*Hometask 25*" << std::endl;
+	std::cout << "*Hometask 26*" << std::endl;
 
 	std::ifstream iFile;
-	iFile.open( SOURCE_DIR "/sources/25/input.txt",
+	iFile.open( SOURCE_DIR "/sources/26/input.txt",
 			std::ios::in | std::ios::binary);
 
-	if (!iFile.is_open()) {
-		std::cout << "cannot open input bin file" << std::endl;
-		return false;
-	}
-
-	std::string sBuff;
-	uint32_t tp;
-	uint32_t tm;
-	uint32_t ln;
-	uint32_t messageSize;
-	dataStruct_t dataRecord;
-	uint32_t currentTime = 0;
-	uint32_t secondsCount = 0;
-	static const int buffSize = 2048;
-	std::map<int, int> buffersSizes;
-	std::map<int, int> messagesCount;
-
-	while (!iFile.eof()) {
-		iFile.read((char*) &tp, sizeof tp);
-		iFile.read((char*) &tm, sizeof tm);
-		iFile.read((char*) &ln, sizeof ln);
-
-		char* buff = new char[ln];
-		iFile.read(buff, ln);
-		sBuff = buff;
-		sBuff = sBuff.substr(0, ln);
-		delete[] buff;
-
-		if ((iFile.eof())) {
-			continue;
-		}
-
-		//new second
-		if (currentTime != tm) {
-			currentTime = tm;
-			secondsCount++;
-			buffersSizes.clear();
-
-			std::cout << "--new second--" << std::endl;
-		}
-
-		messageSize = sizeof tp + sizeof tm + sizeof ln + (sizeof(char)) * ln;
-		std::cout << "SZ[" << messageSize << "]\t";
-
-		std::cout << "tp:" << tp << "\ttm:" << tm << "\tln:" << ln << "\t["
-				<< sBuff << "]\t\t";
-
-		if (!buffersSizes.count(tp)) {
-			buffersSizes[tp] = buffSize - messageSize;
-			messagesCount[tp] += 1;
-		} else {
-			if (buffersSizes[tp] - messageSize >= 0) {
-				buffersSizes[tp] = buffersSizes[tp] - messageSize;
-				messagesCount[tp] += 1;
-			} else {
-				std::cout << "cannot save message" << std::endl;
-			}
-		}
-		std::cout << "buff left [" << buffersSizes[tp] << "] for mt[" << tp << "]"
-				<< std::endl;
-	}
-
-	iFile.close();
-
-	std::cout << "\n--report--" << std::endl;
-	std::cout << "secCount\t" << secondsCount << std::endl;
-	std::cout << "--msg stored--" << std::endl;
-	std::cout << "[type]\tsaved\tavg" << std::endl;
-	for (std::map<int, int>::const_iterator it = messagesCount.begin();
-			it != messagesCount.end(); ++it) {
-		std::cout << "[" << it->first << "]\t" << it->second << "\t"
-				<< (double) it->second / secondsCount << std::endl;
-	}
-
 	std::ofstream oFile;
-	oFile.open( SOURCE_DIR "/sources/25/output.txt", std::ios::out | std::ios::binary);
+	oFile.open( SOURCE_DIR "/sources/26/output.txt",
+			std::ios::out | std::ios::binary);
 
-	if (!oFile.is_open()){
-		std::cout << "cannot open output file" << std::endl;
+	if (!iFile.is_open() || !oFile.is_open()) {
+		std::cout << "cannot open bin files" << std::endl;
 		return 1;
 	}
 
-	double avgMsg;
-	for (std::map<int, int>::const_iterator it = messagesCount.begin();
-			it != messagesCount.end(); ++it) {
-		oFile.write( (char*) &it->first, sizeof it->first);
-		avgMsg = (double) it->second / secondsCount;
-		oFile.write( (char*) &avgMsg, sizeof avgMsg );
+	static const uint32_t stringSize = 9;
+	static const uint32_t dateLength = 8;
+	std::string stockName;
+	std::string date;
+	double price;
+	double vwap;
+	uint32_t volume;
+	double f1;
+	double t1;
+	double f2;
+	double f3;
+	double f4;
+
+	char buff[stringSize];
+
+	while (!iFile.eof()) {
+		//input
+		iFile.read( (char*)&buff, stringSize);
+		stockName = buff;
+		stockName = stockName.substr(0, stringSize);
+
+		iFile.read( (char*)&buff, dateLength);
+		date = buff;
+		date = date.substr(0, dateLength);
+
+		iFile.read((char*) &price, sizeof price);
+		iFile.read((char*) &vwap, sizeof vwap);
+		iFile.read((char*) &volume, sizeof volume);
+		iFile.read((char*) &f1, sizeof f1);
+		iFile.read((char*) &t1, sizeof t1);
+		iFile.read((char*) &f2, sizeof f2);
+		iFile.read((char*) &f3, sizeof f3);
+		iFile.read((char*) &f4, sizeof f4);
+
+		if (iFile.eof()){
+			continue;
+		}
+
+		std::cout << "IN:\tsn[" << stockName << "]\tdt[" << date
+				<< "]\tpr:" << price
+				<< "\tvw:" << vwap
+				<< "\tvl:" << volume
+				<< "\tf1:" << f1
+				<< "\tt1:" << t1
+				<< "\tf2:" << f2
+				<< "\tf3:" << f3
+				<< "\tf4:" << f4
+				<< std::endl;
+
+		//output
+		stockName.append("\0\0\0\0\0\0\0\0\0");
+		stockName = stockName.substr(0, stringSize);
+		std::cout << "OUT\t[" << stockName.c_str() << "]\t";
+
+		uint32_t days = ymd2days(date);
+		std::cout << ymd2days(date) << "\t";
+		std::cout << vwap << "\t" << volume << "\t" << f2 << std::endl;
+
+		oFile.write( (char*)stockName.c_str(), stringSize );
+		oFile.write( (char*)&days, sizeof days);
+		oFile.write( (char*)&vwap, sizeof vwap);
+		oFile.write( (char*)&volume, sizeof volume);
+		oFile.write( (char*)&f2, sizeof f2);
 	}
+
+	iFile.close();
 	oFile.close();
 
 	std::cout << "\n--EOF--" << std::endl;
