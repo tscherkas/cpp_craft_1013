f35fa19248fecbd5e725be09e992ec970704d3ca
alstrokov <strokov.aleksandr@gmail.com>
2013-10-24 09:38:00 +0300

    big homework folders restruct, add homework3 stub

---
+++ solutions/al_strokov/homeworks/3/sources/34/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char** argv){
+    std::cout << "Homework34" << std::endl;
+
+    return 0;
+}
---
+++ solutions/al_strokov/homeworks/3/sources/binReader/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char** argv){
+    std::cout << "binReader" << std::endl;
+
+    return 0;
+}
