3ddbb55f055567e08ea6452bbdc59f8d385e23aa
AndrewZh <hotsmileband@gmail.com>
2013-10-27 22:29:07 +0300

    Finished thread group setup in trades_filter

--- solutions/andrey_zhilka/week3/sources/3_4/trades_filter.cpp
+++ solutions/andrey_zhilka/week3/sources/3_4/trades_filter.cpp
@@ -6,49 +6,58 @@
 #include <boost/filesystem.hpp>
 #include <boost/thread.hpp>
 
-void filterMessages(const std::ifstream&);
+void filterMessages(std::istream*);
 
 static const size_t max_file_number = 999;
 
 int main()
 {
 	boost::thread_group filters;
-	std::ostringstream strim;
+	std::stringstream strim;
 	for ( size_t i = 0; i < max_file_number; i++ )
 	{
-		strim << std::setfill('0') << std::ios::width(3);
+		strim.width( 3 );
+		strim << std::setfill( '0' );
 		strim << i;
 		
-		const char *input_file_path = SOURCE_DIR "/tests/2_4/input_" strim.str().c_str() ".txt";
+		std::string number;
+		strim >> number;
+
+		char input_file_path[] = SOURCE_DIR "/tests/2_4/input_";
+		strcat( input_file_path, number.c_str() );
+		strcat( input_file_path, ".txt");
 
 		boost::filesystem2::path next_file(input_file_path);
 
 		if ( boost::filesystem2::exists( next_file ) )
 		{
-
+			std::ifstream input( input_file_path, std::ios::in | std::ios::binary );
+			filters.add_thread( new boost::thread( &filterMessages, &input ) );
 		}
 		else 
 		{
 			break;
 		}
 
+		filters.join_all();
+		
+
 		strim.clear();
 	}
-	
-	filterMessages();
 
 	return 0;
 }
 
-void filterMessages(const std::ifstream& input) {
+void filterMessages(std::istream* input) {
 	unsigned maxT = 0;
 	
 	Message new_message;
+	std::ofstream output;
 
 	while ( true ) {
-		input >> new_message;
+		*input >> new_message;
 
-		if ( input.eof() )
+		if ( input->eof() )
 		{
 			break;
 		}
@@ -63,7 +72,4 @@ void filterMessages(const std::ifstream& input) {
 			maxT = ( maxT < new_message.getTime() ) ? new_message.getTime() : maxT;
 		}
 	}
-
-	input.close();
-	output.close();
 }
