a57821fa35ef200f4554e7b37c212a12d8b53beb
alstrokov <strokov.aleksandr@gmail.com>
2013-10-28 16:02:13 +0300

    Binary writer added

---
+++ solutions/al_strokov/homeworks/3/sources/binFileRW/binFileWriter.cpp
@@ -0,0 +1,20 @@
+#include "binFileWriter.h"
+
+
+binFileWriter::binFileWriter(const std::string& filename):
+    fName(filename), fileOk(false){
+    ofs.open( filename.c_str(), std::ios::binary | std::ios::out );
+}
+
+
+void binFileWriter::writeRawValue(std::string& value, const size_t length){
+	ofs.write( value.c_str(), length);
+}
+
+
+void binFileWriter::writeDataStruct1(dataStruct1_t& dataStruct){
+	writeRawValue(dataStruct.type);
+	writeRawValue(dataStruct.time);
+	writeRawValue(dataStruct.len);
+	writeRawValue(dataStruct.msg, dataStruct.len);
+}
---
+++ solutions/al_strokov/homeworks/3/sources/binFileRW/binFileWriter.h
@@ -0,0 +1,36 @@
+#ifndef BINFILEWRITER_INCLUDED
+#define BINFILEWRITER_INCLUDED
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <stdint.h>
+#include "dataStructs.h"
+
+class binFileWriter{
+private:
+    std::string fName;
+    std::fstream ofs;
+    bool fileOk;
+
+    binFileWriter(const binFileWriter& that);
+    binFileWriter operator=(const binFileWriter& that);
+
+public:
+    binFileWriter(const std::string& filename);
+
+    template<class T>
+    void writeRawValue(T& value);
+    void writeRawValue(std::string& value, const size_t length);
+
+    void writeDataStruct1(dataStruct1_t& dataStruct);
+};
+
+
+template<class T>
+void binFileWriter::writeRawValue(T& value){
+   ofs.write( (char*)value, sizeof value);
+}
+
+
+#endif
