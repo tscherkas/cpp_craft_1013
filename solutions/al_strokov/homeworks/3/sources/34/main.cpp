816d4796e04323a051cf4c0b9e79b694022dacb5
alstrokov <strokov.aleksandr@gmail.com>
2013-10-28 14:22:25 +0300

    init Homework 3.

---
+++ solutions/al_strokov/homeworks/3/sources/34/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char** argv) {
+	std::cout << "*Homework34*" << std::endl;
+
+	std::string filename = SOURCE_DIR "/sources/34/input.txt";
+
+	std::cout << "---===over===---" << std::endl;
+	return 0;
+}
