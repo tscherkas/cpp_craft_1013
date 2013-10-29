93a1183e19a0ac585441b6f0988bd6b2f4967070
AndrewZh <hotsmileband@gmail.com>
2013-10-29 22:03:25 +0300

    Finished 3_6

--- solutions/andrey_zhilka/week3/sources/3_6/StockDealer.h
+++ solutions/andrey_zhilka/week3/sources/3_6/StockDealer.h
@@ -2,17 +2,15 @@
 #define _STOCK_DEALER_H_
 
 #include <fstream>
+#include <map>
+#include <string>
+#include <vector>
 
-static const char* input_file_path = SOURCE_DIR "/tests/2_6/2.6_example.in";
-static const char* output_file_path = SOURCE_DIR "/tests/2_6/2.6_example.out";
+static const char* input_file_path = SOURCE_DIR "/tests/3_6/3.6_example.in";
 
 class StockDealer
 {
-private :
-	std::ifstream stock_info;
-	std::ofstream info_output;
-
-public :
+public:
 	struct StockDeal
 	{
 		char* stock_name;
@@ -27,14 +25,30 @@ public :
 		bool read_deal( std::ifstream& );
 		void write_deal( std::ofstream& );
 
-	private :
-		StockDeal(const StockDeal&){}
-	};
+		StockDeal(const StockDeal& deal)
+		{
+			this->date = deal.date;
+			this->price = deal.price;
+			this->volume = deal.volume;
+			this->f2 = deal.f2;
 
+			this->stock_name = new char[ strlen(deal.stock_name) + 1 ];
+			strcpy( this->stock_name, deal.stock_name ); 
+		}
+	};
+	
 	StockDealer();
 	~StockDealer();
 
 	void process_info();
+	void sort_deal( const StockDeal& );
+	void output_catalogue();
+	void write_stock( std::string );
+
+private :
+	std::ifstream stock_info;
+	std::map< std::string, std::vector< StockDealer::StockDeal > > deals_catalogue;
+
 };
 
 #endif
