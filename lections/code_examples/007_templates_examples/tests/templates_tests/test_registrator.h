f9370c8e6cb25c0d815c9e86b7539c9840c92f25
Ivan Sidarau <ivan.sidarau@disfa.com>
2013-10-30 18:51:55 +0300

    adding function examples

---
+++ lections/code_examples/007_templates_examples/tests/templates_tests/test_registrator.h
@@ -0,0 +1,12 @@
+#include <boost/test/test_tools.hpp>
+
+namespace templates
+{
+	namespace tests_
+	{
+		void function_1_tests();
+		void function_2_tests();
+		void function_3_tests();
+		void function_4_tests();
+	}
+}
