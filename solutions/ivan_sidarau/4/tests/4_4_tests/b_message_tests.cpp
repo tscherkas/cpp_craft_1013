b54dd4d6d333d652ae8d4a53c54a41e2fecc746c
Ivan Sidarau <ivan.sidarau@disfa.com>
2013-10-30 16:06:52 +0300

    fixing 3 example (according to CMake 2.8.12), fixing test for 4 example

--- solutions/ivan_sidarau/4/tests/4_4_tests/b_message_tests.cpp
+++ solutions/ivan_sidarau/4/tests/4_4_tests/b_message_tests.cpp
@@ -18,7 +18,7 @@ void task4_4::tests_::b_message_constructor_tests()
 	BOOST_CHECK_NO_THROW( b_message::create_message( test_stream ) );
 	
 	test_stream.clear();
-	test_stream << "16 012345678912345";
+	test_stream << "16 0123456789123456";
 	message_ptr message = b_message::create_message( test_stream );
 
 	BOOST_CHECK_EQUAL( message->type(), 'B' );
