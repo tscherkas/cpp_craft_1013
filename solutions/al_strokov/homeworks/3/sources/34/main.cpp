e0e397a5c54fa8af0d1f5661a3b55f62e27c8742
alstrokov <strokov.aleksandr@gmail.com>
2013-10-31 16:55:41 +0300

    main multithread

--- solutions/al_strokov/homeworks/3/sources/34/main.cpp
+++ solutions/al_strokov/homeworks/3/sources/34/main.cpp
@@ -6,12 +6,12 @@
 #include <boost/thread/thread.hpp>
 #include <boost/filesystem/operations.hpp>
 #include <boost/filesystem/path.hpp>
+#include <strstream>
 
+uint32_t tmp[] = { 1u, 2u, 3u, 4u };
+static const std::set<uint32_t> acceptableOperations(&tmp[0], &tmp[0] + 4);
 
-uint32_t tmp[] = {1u, 2u, 3u, 4u};
-static const std::set<uint32_t> acceptableOperations(&tmp[0], &tmp[0]+ 4);
-
-void processor(const std::string& ifname, const std::string& ofname){
+void processor(const std::string& ifname, const std::string& ofname) {
 	binFileReader bfr(ifname);
 
 	if (bfr.fileOk) {
@@ -21,11 +21,11 @@ void processor(const std::string& ifname, const std::string& ofname){
 		return;
 	}
 
-    binFileWriter bfw(ofname);
+	binFileWriter bfw(ofname);
 	if (bfw.fileOk) {
-		std::cout << "o fileok" << std::endl;
+		//std::cout << "o fileok" << std::endl;
 	} else {
-		std::cout << "o fileNOTok" << std::endl;
+		//std::cout << "o fileNOTok" << std::endl;
 		return;
 	}
 
@@ -33,30 +33,29 @@ void processor(const std::string& ifname, const std::string& ofname){
 	bool isRecordValid;
 	dataStruct1_t ds1;
 
-	while(bfr.readDataStruct(ds1)){
-		std::cout << "type" << ds1.type << "\ttime" << ds1.time << "\t["
-					<< ds1.msg << "]=" << ds1.len << std::endl;
+	while (bfr.readDataStruct(ds1)) {
+//		std::cout << "type" << ds1.type << "\ttime" << ds1.time << "\t["
+//				<< ds1.msg << "]=" << ds1.len << std::endl;
 
 		isRecordValid = true;
 
-		if ( (!acceptableOperations.count(ds1.type)) ||
-				(ds1.time + 2 <= maxTime)/* ||
-				(bfr.fileOk) */){
+		if ((!acceptableOperations.count(ds1.type)) || (ds1.time + 2 <= maxTime)/* ||
+		 (bfr.fileOk) */) {
 			isRecordValid = false;
-			std::cout << "FILTERED ";
+//			std::cout << "FILTERED ";
 		}
 
-		if ( maxTime < ds1.time ){
+		if (maxTime < ds1.time) {
 			maxTime = ds1.time;
 		}
-        
-        if (isRecordValid){
-            bfw.writeDataStruct(ds1);
-        }
+
+		if (isRecordValid) {
+			bfw.writeDataStruct(ds1);
+		}
 	}
 }
 
-void x(){
+void x() {
 	std::cout << "x " << std::endl;
 }
 
@@ -66,21 +65,33 @@ int main(int argc, char** argv) {
 	std::string filenamei = SOURCE_DIR "/sources/34/input.txt";
 	std::string filenameo = SOURCE_DIR "/sources/34/output.txt";
 
-//    processor(filename,SOURCE_DIR "/sources/34/output.txt");
 
-	//boost::thread th( &processor(filenamei, filenameo) );
+    boost::thread_group tg;
+	static const size_t maxFileIndex = 999;
+	std::stringstream sstr;
 
-	if(boost::filesystem::exists(filenamei)){
-		std::cout << "input exists" << std::endl;
+	for(size_t i = 0; i < maxFileIndex; i++){
+		sstr << SOURCE_DIR "/sources/34/input_" << i << ".txt";
+		if(boost::filesystem::exists(sstr.str())){
+			filenamei = sstr.str();
+			sstr.str("");
+			sstr << SOURCE_DIR "/sources/34/output_" << i << ".txt";
+			filenameo = sstr.str();
+			std::cout << filenamei << "--->" << filenameo << std::endl;
+			tg.create_thread( boost::bind(&processor, filenamei, filenameo) );
+		}
+		sstr.str("");
 	}
-	else
-	{
-		std::cout << "input NOT exists" << std::endl;
-	}
 
+//	if (boost::filesystem::exists(filenamei)) {
+//		std::cout << "input exists" << std::endl;
+//	} else {
+//		std::cout << "input NOT exists" << std::endl;
+//	}
 
-	boost::thread th( &processor, filenamei, filenameo );
-	th.join();
+    tg.join_all();
+    
+    
 
 	std::cout << "---===over===---" << std::endl;
 	return 0;
