e141ba2359c7c3aa13f34241a46886ba65f619c4
alstrokov <strokov.aleksandr@gmail.com>
2013-11-01 08:14:28 +0300

    task34 done

--- solutions/al_strokov/homeworks/3/sources/34/main.cpp
+++ solutions/al_strokov/homeworks/3/sources/34/main.cpp
@@ -14,18 +14,12 @@ static const std::set<uint32_t> acceptableOperations(&tmp[0], &tmp[0] + 4);
 void processor(const std::string& ifname, const std::string& ofname) {
 	binFileReader bfr(ifname);
 
-	if (bfr.fileOk) {
-		std::cout << "i fileok" << std::endl;
-	} else {
-		std::cout << "i fileNOTok" << std::endl;
+	if (!bfr.fileOk){
 		return;
 	}
 
 	binFileWriter bfw(ofname);
-	if (bfw.fileOk) {
-		//std::cout << "o fileok" << std::endl;
-	} else {
-		//std::cout << "o fileNOTok" << std::endl;
+	if (!bfw.fileOk) {
 		return;
 	}
 
@@ -34,15 +28,10 @@ void processor(const std::string& ifname, const std::string& ofname) {
 	dataStruct1_t ds1;
 
 	while (bfr.readDataStruct(ds1)) {
-//		std::cout << "type" << ds1.type << "\ttime" << ds1.time << "\t["
-//				<< ds1.msg << "]=" << ds1.len << std::endl;
-
 		isRecordValid = true;
 
-		if ((!acceptableOperations.count(ds1.type)) || (ds1.time + 2 <= maxTime)/* ||
-		 (bfr.fileOk) */) {
+		if ((!acceptableOperations.count(ds1.type)) || (ds1.time + 2 <= maxTime)) {
 			isRecordValid = false;
-//			std::cout << "FILTERED ";
 		}
 
 		if (maxTime < ds1.time) {
@@ -55,9 +44,6 @@ void processor(const std::string& ifname, const std::string& ofname) {
 	}
 }
 
-void x() {
-	std::cout << "x " << std::endl;
-}
 
 int main(int argc, char** argv) {
 	std::cout << "*Homework34*" << std::endl;
@@ -83,15 +69,7 @@ int main(int argc, char** argv) {
 		sstr.str("");
 	}
 
-//	if (boost::filesystem::exists(filenamei)) {
-//		std::cout << "input exists" << std::endl;
-//	} else {
-//		std::cout << "input NOT exists" << std::endl;
-//	}
-
     tg.join_all();
-    
-    
 
 	std::cout << "---===over===---" << std::endl;
 	return 0;
