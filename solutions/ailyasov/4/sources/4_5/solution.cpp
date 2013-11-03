efa2c4279fc5d20dd8625c9fcd2c9c209d72c7fc
ailyasov <artem.ilyasov@gmail.com>
2013-11-03 21:22:10 +0300

    homework4_5 small update

--- solutions/ailyasov/4/sources/4_5/solution.cpp
+++ solutions/ailyasov/4/sources/4_5/solution.cpp
@@ -10,21 +10,14 @@
 
 using boost::uint32_t;
 
-static int DEFAULT_VALUE = 0;
-
-int deref_iterator_or_default(std::vector<int>::const_iterator it, std::vector<int> const& v)
-{ 
-    return (it == v.end()) ? DEFAULT_VALUE : *it;
-}
-
 void task4_5::solution::add_min_max(data_type::const_iterator it)
 {
     std::vector<int> const& v = *it;
     if(!v.empty())
     {
-        int min = std::numeric_limits<int>().max();
-        int max = std::numeric_limits<int>().min();
-        for(std::vector<int>::const_iterator itt = v.begin(); itt != v.end(); ++itt)
+        int min = *v.begin();
+        int max = *v.begin(); 
+        for(std::vector<int>::const_iterator itt = v.begin() + 1; itt != v.end(); ++itt)
         {
             if(*itt < min)
                 min = *itt;
@@ -37,20 +30,25 @@ void task4_5::solution::add_min_max(data_type::const_iterator it)
     }
 } 
 
-task4_5::solution::solution( const data_type& data ) : data_(data)
+task4_5::solution::solution( const data_type& data ) : data_(data), max_(0), min_(0)
 {
-    max_values_.reserve(data.size());
-    min_values_.reserve(data.size());
+    max_values_.reserve(data_.size());
+    min_values_.reserve(data_.size());
 
     boost::thread_group t_group;
     for(data_type::const_iterator it = data_.begin(); it != data_.end(); ++it)
-    {
         t_group.create_thread(boost::bind(&task4_5::solution::add_min_max,this,it));
-    } 
     t_group.join_all();
 
-    min_ = deref_iterator_or_default(std::min_element(min_values_.begin(), min_values_.end()), min_values_); 
-    max_ = deref_iterator_or_default(std::max_element(max_values_.begin(), max_values_.end()), max_values_); 
+    if(!min_values_.empty())
+    {
+        min_ = *std::min_element(min_values_.begin(), min_values_.end());
+    }
+
+    if(!max_values_.empty())
+    {
+        max_ = *std::max_element(max_values_.begin(), max_values_.end()); 
+    }
 } 
 
 int task4_5::solution::get_min() const
