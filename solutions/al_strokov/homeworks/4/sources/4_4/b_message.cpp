cc265071b40f321016d6fe41d25dd79b3310d515
alstrokov <strokov.aleksandr@gmail.com>
2013-11-05 09:59:48 +0300

    task44 done

--- solutions/al_strokov/homeworks/4/sources/4_4/b_message.cpp
+++ solutions/al_strokov/homeworks/4/sources/4_4/b_message.cpp
@@ -2,19 +2,19 @@
 
 #include <sstream>
 
-task4_4::b_message::b_message( std::istream& inp )
+task4_4::b_message::b_message( std::istream& inp ):
+	length_(0)
 {
-	inp.seekg(0, inp.beg);
-
 	inp >> length_;
 	if(inp.eof()){
 		throw std::logic_error("bad input stream, a_message cannot be readed");
 	}
 
 	content_ = new char[ length_ ];
+	inp.read( content_, 1 );
 	inp.read( content_, length_ );
-	std::cout << "readed [" << inp.gcount() << "]charz; need lenght:" << length_ << "str:"<< content_ << std::endl;
-	if(inp.gcount() < length_){
+
+	if( (size_t)inp.gcount() < length_){
 		throw std::logic_error("bad input stream, not enough data");
 	}
 }
@@ -35,9 +35,7 @@ const char task4_4::b_message::type() const
 }
 const std::string task4_4::b_message::str() const
 {
-	//b_message(16|0123456789123456)
 	std::stringstream sBuff;
 	sBuff << "b_message(" << length_ << "|" << std::string(content_, length_) << ")";
-	std::cout << sBuff.str() << std::endl;
 	return sBuff.str();
 }
