cbe127e743c5c01b36d5d6cb02dc7b76753e1348
Aliaksandr Talochka <Aljaksandr@gmail.com>
2013-11-11 00:14:17 +0300

    unthreaded 4_5 solved

--- solutions/Aliaksandr_Talochka/4/sources/4_5/solution.cpp
+++ solutions/Aliaksandr_Talochka/4/sources/4_5/solution.cpp
@@ -1,14 +1,34 @@
 #include "solution.h"
 
+
 task4_5::solution::solution( const data_type& data )
+	: Max(0), Min(0)
 {
+	if(data.size())
+		FindMinMax(data);
 }
 
 int task4_5::solution::get_min() const
 {
-	return 0;
+	return Min;
 }
 int task4_5::solution::get_max() const
 {
-	return 0;
+	return Max;
+}
+
+void task4_5::solution::FindMinMax(const data_type& Dat)
+{
+	Max = std::numeric_limits<int>::min();
+	Min = std::numeric_limits<int>::max();
+
+	data_type::const_iterator it;
+	for(it = Dat.begin(); it != Dat.end(); ++it)
+	{
+		std::pair< std::vector< int >::const_iterator, std::vector< int >::const_iterator > result = boost::minmax_element(it->begin(), it->end());
+
+		if(*result.first < Min) Min = *result.first;
+		if(*result.second > Max) Max = *result.second;
+		
+	}
 }
--- solutions/Aliaksandr_Talochka/4/sources/4_5/solution.h
+++ solutions/Aliaksandr_Talochka/4/sources/4_5/solution.h
@@ -2,17 +2,28 @@
 #define _TASK4_5_SOLUTION_H_
 
 #include <vector>
+#include <limits>
+#include <boost\noncopyable.hpp>
+#include <boost\thread.hpp>
+#include <boost\algorithm/minmax_element.hpp>
 
 namespace task4_5
 {
 	typedef std::vector< std::vector< int > > data_type;
 
-	class solution
+	class solution: virtual protected boost::noncopyable
 	{
 	public:
 		explicit solution( const data_type& data );
 		int get_min() const;
 		int get_max() const;
+	private:
+		int Max, Min;
+		boost::thread_group tg;
+
+		void FindMinMax(const data_type& Dat);
+
+
 	};
 }
 
