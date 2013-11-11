204bfac2856d68f80dfec91c250d9d932ef29f74
Aliaksandr Talochka <Aljaksandr@gmail.com>
2013-11-11 10:19:50 +0300

    4_5 solved

--- solutions/Aliaksandr_Talochka/4/sources/4_5/solution.h
+++ solutions/Aliaksandr_Talochka/4/sources/4_5/solution.h
@@ -3,9 +3,13 @@
 
 #include <vector>
 #include <limits>
+#include <math.h>
 #include <boost\noncopyable.hpp>
 #include <boost\thread.hpp>
 #include <boost\algorithm/minmax_element.hpp>
+
+
+const static size_t NUM_THREADS = 2;
 
 namespace task4_5
 {
@@ -14,14 +18,16 @@ namespace task4_5
 	class solution: virtual protected boost::noncopyable
 	{
 	public:
-		explicit solution( const data_type& data );
+		explicit solution( const data_type& data, const size_t NumThreads = NUM_THREADS);
 		int get_min() const;
 		int get_max() const;
 	private:
 		int Max, Min;
 		boost::thread_group tg;
+		boost::mutex Mtx_MinMax_update;
 
-		void FindMinMax(const data_type& Dat);
+		void FindMinMax(const data_type& Dat, const size_t NumThreads);
+		void ThreadedSearch(const data_type& Dat, size_t shift_Start, size_t shift_End);
 
 
 	};
